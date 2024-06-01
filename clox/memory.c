22f3c9746574effa8aa087a98f7bc0da515fe47e
ycpeng <ycpeng@bu.edu>
2024-06-01 15:33:14 +0800

    basic chunk

---
+++ clox/memory.c
@@ -0,0 +1,17 @@
+//
+// Created by Yanchong Peng on 5/29/24.
+//
+#include <stdlib.h>
+
+#include "memory.h"
+
+void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+    if (newSize == 0) {
+        free(pointer);
+        return NULL;
+    }
+
+    void* result = realloc(pointer, newSize);
+    if (result == NULL) exit(1);
+    return result;
+}
